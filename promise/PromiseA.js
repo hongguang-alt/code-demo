697c7f9cab7daf21e0dc0583f534432eb25fa67c
红光 <2273049646@qq.com>
2023-08-28 23:58:07 +0800

    feat: 新增 Promise demo

---
+++ promise/PromiseA.js
@@ -0,0 +1,134 @@
+// 手动实现一个 Promise
+
+/**
+ * 实现的几个点
+ * 1. 三种状态
+ * 2. then 方法返回 promise，可以链式调用
+ * 3. 关于规范处理参数
+ */
+
+/**
+ * 遇到的几个问题
+ * 第一个promise的错误处理怎么处理
+ * 后面then里面的错误处理怎么处理
+ */
+
+const PENDING = "pending";
+const RESLOVED = "resloved";
+const REJECTED = "rejected";
+
+module.exports = class PromiseA {
+  constructor(executor) {
+    this.status = PENDING;
+    this.successValue = undefined;
+    this.errorValue = undefined;
+    this.onFulfilledCallbacks = [];
+    this.onRejectedCallbacks = [];
+    try {
+      executor(this.reslove.call(this), this.reject.call(this));
+    } catch (e) {
+      this.reject(e);
+    }
+  }
+  reslove(val) {
+    if (this.status !== PENDING) return;
+    this.status = RESLOVED;
+    this.successValue = val;
+    this.onFulfilledCallbacks.forEach((fn) => fn(this.successValue));
+  }
+  reject(val) {
+    if (this.status !== PENDING) return;
+    this.status = REJECTED;
+    this.errorValue = val;
+    this.onRejectedCallbacks.forEach((fn) => fn(this.errorValue));
+  }
+
+  then(onFulfilled, onRejected) {
+    onFulfilled = typeof onFulfilled === "function" ? onFulfilled : (v) => v;
+    onRejected =
+      typeof onRejected === "function"
+        ? onRejected
+        : (err) => {
+            throw err;
+          };
+    // 添加 回调函数，返回一个 promise 是上一次的结果
+    let p = new PromiseA((reslove, reject) => {
+      // 进行执行了
+      if (this.status === RESLOVED) {
+        try {
+          let x = onFulfilled(this.successValue);
+          resolvePromise(p, x, reslove, reject);
+        } catch (e) {
+          reject(e);
+        }
+      }
+
+      if (this.status === REJECTED) {
+        try {
+          let x = onRejected(this.errorValue);
+          resolvePromise(p, x, reslove, reject);
+        } catch (e) {
+          reject(e);
+        }
+      }
+
+      if (this.status === PENDING) {
+        this.onFulfilledCallbacks.push((val) => {
+          try {
+            // 执行
+            let res = onFulfilled(val);
+            resolvePromise(p, res, reslove, reject);
+          } catch (e) {
+            reject(e);
+          }
+        });
+        this.onRejectedCallbacks.push((val) => {
+          try {
+            let res = onRejected(val);
+            resolvePromise(p, res, reslove, reject);
+          } catch (e) {
+            reject(e);
+          }
+        });
+      }
+    });
+    return p;
+  }
+};
+
+function resolvePromise(p, x, reslove, reject) {
+  if (p === x) {
+    return reject(
+      new new TypeError("Chaining cycle detected for promise #<Promise>")()
+    );
+  }
+  let called;
+  if ((typeof x === "object" && x !== null) || typeof x === "function") {
+    try {
+      let then = x.then;
+      if (typeof then === "function") {
+        then.call(
+          x,
+          (y) => {
+            if (called) return;
+            called = true;
+            // 继续解析到对应的 then
+            resolvePromise(p, y, resolve, reject);
+          },
+          (r) => {
+            if (called) return;
+            called = true;
+            reject(r);
+          }
+        );
+        console.log("执行then方法");
+      } else {
+        reslove(x);
+      }
+    } catch (e) {
+      reject(e);
+    }
+  } else {
+    reslove(x);
+  }
+}
---
+++ promise/index.js
@@ -0,0 +1,33 @@
+const PromiseA = require("./PromiseAV");
+
+// 执行 promise
+const p1 = new PromiseA((reslove, reject) => {
+  console.log("create promise");
+  setTimeout(() => {
+    reslove("success");
+    // reject("err");
+  }, 3000);
+})
+  .then(
+    (res) => {
+      console.log(res, "success");
+      return "xx";
+    },
+    (err) => {
+      console.log(err, "err");
+      return "xxo";
+    }
+  )
+  .then(
+    (res) => {
+      console.log(res, "xx");
+      return new PromiseA((res) => res("adsfasdf"));
+      // return "adsfasdf";
+    },
+    (err) => {
+      console.log(err, "xxo");
+    }
+  )
+  .then((res) => {
+    console.log(res);
+  });
